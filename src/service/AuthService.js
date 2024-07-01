ae08432e8e2ba3eb1f142b8e20190be42514895c
chesterrey <rey.tan@student.ateneo.edu>
2024-07-01 12:33:28 +0800

    env api

--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -1,4 +1,4 @@
-const API_URL = "https://api.strength-training.xyz";
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api/auth`;
 
 class AuthService {
   async login(form) {
--- src/service/ExerciseService.js
+++ src/service/ExerciseService.js
@@ -1,4 +1,4 @@
-const API_URL = "https://api.strength-training.xyz/api";
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api`;
 
 class ExerciseService {
   async addExercise(form) {
