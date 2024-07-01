f6682c560bd609a8088f8448fb141cd20eace36d
chesterrey <rey.tan@student.ateneo.edu>
2024-07-01 11:31:21 +0800

    api url

--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000";
+const API_URL = "https://api.strength-training.xyz";
 
 class AuthService {
   async login(form) {
--- src/service/ExerciseService.js
+++ src/service/ExerciseService.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000/api";
+const API_URL = "https://api.strength-training.xyz/api";
 
 class ExerciseService {
   async addExercise(form) {
