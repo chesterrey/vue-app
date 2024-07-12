eb865f14d8710ae96188750ce641400208233e00
chesterrey <rey.tan@student.ateneo.edu>
2024-07-12 16:48:18 +0800

    update navbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { useRouter } from 'vue-router';
+import { useRouter } from "vue-router";
 import { ref } from "vue";
 import useAuthStore from "../store/auth.js";
-import useBlockStore from '../store/blocks';
+import useBlockStore from "../store/blocks";
 
-import Avatar from 'primevue/avatar';
-import Menu from 'primevue/menu';
-import Button from 'primevue/button';
+import Avatar from "primevue/avatar";
+import Menu from "primevue/menu";
+import Button from "primevue/button";
 
 const router = useRouter();
 
@@ -14,43 +14,62 @@ const route = router.currentRoute.value;
 
 const menu = ref();
 const items = ref([
-    {
-        label: `${useAuthStore().user.value}`,
-        items: [
-            {
-                label: 'Logout',
-                command: () => {
-                    useAuthStore().destroySession();
-                    useBlockStore().reset();
-                    router.push({ name: 'auth' });
-                }
-            },
-        ]
-    }
+  {
+    label: `${useAuthStore().user.value}`,
+    items: [
+      {
+        label: "Logout",
+        command: () => {
+          useAuthStore().destroySession();
+          useBlockStore().reset();
+          router.push({ name: "auth" });
+        },
+      },
+    ],
+  },
 ]);
 
 const toggle = (event) => {
-    menu.value.toggle(event);
+  menu.value.toggle(event);
 };
 </script>
 <template>
-    <main class="relative">
-        <header class="shadow-md py-6 px-8 flex justify-between items-center border-b">
-            <h1 class="text-lg font-bold">Training App</h1>
-            <Button icon="pi pi-user" rounded outlined @click="toggle" />
-            <Menu ref="menu" id="overlay_menu" :model="items" :popup="true" />
-        </header>
-        <slot></slot>
-        <nav class="fixed bottom-0 grid grid-cols-3 w-full min-h-20 border-t bg-white">
-            <router-link to="/home" class="p-4 pb-6 text-center" :class="{'bg-gray-200': route.name === 'home'}">
-                <i class="pi pi-flag"></i><br><span>Current</span>
-            </router-link>
-            <router-link to="/blocks" class="p-4 pb-6 text-center" :class="{'bg-gray-200': route.name === 'blocks' || route.name === 'block-detail'}">
-                <i class="pi pi-folder-open"></i><br><span>Blocks</span>
-            </router-link>
-            <router-link to="/progress" class="p-4 pb-6 text-center" :class="{'bg-gray-200': route.name === 'progress'}">
-                <i class="pi pi-chart-line"></i><br><span>Progress</span>
-            </router-link>
-        </nav>
-    </main>
+  <main class="relative">
+    <header
+      class="shadow-md py-6 px-8 flex justify-between items-center border-b"
+    >
+      <h1 class="text-lg font-bold">Training App</h1>
+      <Button icon="pi pi-user" rounded outlined @click="toggle" />
+      <Menu ref="menu" id="overlay_menu" :model="items" :popup="true" />
+    </header>
+    <slot></slot>
+    <nav
+      class="fixed bottom-0 grid grid-cols-3 w-full min-h-20 border-t bg-white font-semibold"
+    >
+      <router-link
+        to="/home"
+        class="pt-2 pb-8 text-center"
+        :class="{ 'text-primary': route.name === 'home' }"
+      >
+        <i class="pi pi-flag"></i><br /><span>Current</span>
+      </router-link>
+      <router-link
+        to="/blocks"
+        class="pt-2 pb-8 text-center"
+        :class="{
+          'text-primary':
+            route.name === 'blocks' || route.name === 'block-detail',
+        }"
+      >
+        <i class="pi pi-folder-open"></i><br /><span>Blocks</span>
+      </router-link>
+      <router-link
+        to="/progress"
+        class="pt-2 pb-8 text-center"
+        :class="{ 'text-primary': route.name === 'progress' }"
+      >
+        <i class="pi pi-chart-line"></i><br /><span>Progress</span>
+      </router-link>
+    </nav>
+  </main>
 </template>
